1fd9a5503a90f24d565a9ba2508c9235a1c053ae
Ivan Osipov <ivano@aiven.io>
2021-08-04 13:15:35 +0300

    client:update deps and readme

--- client/README.md
+++ client/README.md
@@ -4,3 +4,19 @@ It is possible to create/edit/delete and re-arrange notes.
 Notes saved to local storage of a browser, and the will persists even if connection is lost.
 
 Notes application make use of Socket.io client and changes made locally would be visible for other users.
+
+## How to run locally
+To install necessary dependencies:
+```shell
+yarn
+```
+
+To run app in development mode:
+```shell
+yarn run dev
+```
+## How to test
+To run test suite:
+```shell
+yarn run test
+```
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,62 +1,55 @@
-import { createServer } from "http";
-import { Server, Socket } from "socket.io";
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 import mongoose from 'mongoose';
 
-import { Notes } from "./types";
-import { Note } from "../Note";
+import { Notes } from './types';
 
 // Connect to MongoDB container
-const mongoURI = 'mongodb://0 0.0.0.0:27017/notes';
-try {
-  mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-  });
-} catch (error) {
-  console.error(`Was not able to connect to mongoDB and got error: ${error}`)
-}
+// const mongoURI = 'mongodb://0 0.0.0.0:27017/notes';
+// try {
+//   mongoose.connect(mongoURI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true
+//   });
+// } catch (error) {
+//   console.error(`Was not able to connect to mongoDB and got error: ${error}`)
+// }
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
   cors: {
     // TODO: check if 0.0.0.0 ip address works
     origin: `http://localhost:3000`,
-    methods: ["GET", "POST"]
+    methods: ['GET', 'POST']
   }
 });
 
-io.on("connection", (socket: Socket) => {
+io.on('connection', (socket: Socket) => {
   console.log(`connected: ${socket.id}`);
 
   socket.on('disconnect', () => {
-    console.log(`Disconnected: ${socket.id}`)
+    console.log(`Disconnected: ${socket.id}`);
   });
 
-  socket.on('get-note',(notes:Notes) =>{
-    const data = findOrCreateNote(notes.)
-  })
-
   socket.on('send-notes', (notes: Notes) => {
-    socket.broadcast.emit('received-notes', notes)
+    socket.broadcast.emit('received-notes', notes);
   });
 });
 
-
-async function findOrCreateNote(id: string | null) {
-  const defaultValue = "";
-  if (id === null) return;
-
-  try {
-    const note = await Note.findById(id);
-    if (note) return note;
-    return Note.create({ _id: id, data: defaultValue });
-  } catch (error) {
-    console.error(`Was not able to find or create note and got error:${error}`)
-  }
-
-}
+// async function findOrCreateNote(id: string | null) {
+//   const defaultValue = "";
+//   if (id === null) return;
+//
+//   try {
+//     const note = await Note.findById(id);
+//     if (note) return note;
+//     return Note.create({ _id: id, data: defaultValue });
+//   } catch (error) {
+//     console.error(`Was not able to find or create note and got error:${error}`)
+//   }
+//
+// }
 
 httpServer.listen(5000);
-
