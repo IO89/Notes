efe2f5c56726a2a6593e9f629be41f061554aed0
Ivan Osipov <ivano@aiven.io>
2021-08-06 11:59:21 +0300

    update test for edit note

--- client/src/components/EditNote.spec.tsx
+++ client/src/components/EditNote.spec.tsx
@@ -1,10 +1,15 @@
-import { screen, render, cleanup } from "@testing-library/react";
-import { EditNote } from "./EditNote";
-import { v4 as uuidv4 } from "uuid";
-import userEvent from "@testing-library/user-event";
+import { screen, render, cleanup } from '@testing-library/react';
+import { EditNote } from './EditNote';
+import { v4 as uuidv4 } from 'uuid';
+import userEvent from '@testing-library/user-event';
 
-describe("EditNote", () => {
-  const currentNote = { id: uuidv4(), text: "Note-1", date: "29/07/2021" };
+describe('EditNote', () => {
+  const currentNote = {
+    id: uuidv4(),
+    text: 'Note-1',
+    date: '29/07/2021',
+    order: 1
+  };
   const mockHandleUpdateNote = jest.fn();
   const mockHandleSwitchEditMode = jest.fn();
 
@@ -22,13 +27,13 @@ describe("EditNote", () => {
     cleanup();
   });
 
-  it("should render EditNote component", () => {
-    expect(screen.getByRole("textbox"));
-    expect(screen.getByRole("button", { name: "Update" }));
+  it('should render EditNote component', () => {
+    expect(screen.getByRole('textbox'));
+    expect(screen.getByRole('button', { name: 'Update' }));
   });
 
-  it("should call update function when update button pressed", () => {
-    userEvent.click(screen.getByRole("button", { name: "Update" }));
+  it('should call update function when update button pressed', () => {
+    userEvent.click(screen.getByRole('button', { name: 'Update' }));
     expect(mockHandleUpdateNote).toHaveBeenCalled();
   });
 });
